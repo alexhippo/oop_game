c96d063c31077727f54af5d8c8b0c2cd78c08d81
Alex Hipolito <alexheepo@gmail.com>
2021-05-21 18:01:35 +1000

    Added addPhraseToDisplay() method

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,46 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+
+  /**
+  * Adds letter placeholders to the display when the game starts
+  * @param {String} phrase - phrase to be used for this game
+  */
+  // @todo - Reinstate Start Game overlay 
+  addPhraseToDisplay() {
+    const phraseDisplay = document.getElementById('phrase');
+    const phraseDisplayUl = phraseDisplay.firstElementChild;
+    const phraseArray = this.phrase.split('');
+
+    phraseArray.forEach((char) => {
+      const charLi = document.createElement('li');
+      charLi.textContent = char;
+      if (char !== ' ') {
+        charLi.className = `hide letter ${char}`;
+      } else {
+        charLi.className = 'space';
+      }
+      phraseDisplayUl.appendChild(charLi);
+    })
+  }
+
+  /**
+  * Checks to see if the letter selected by the player matches a letter in the phrase
+  */
+  checkLetter() {
+
+  }
+
+  /**
+  * Reveals the letter(s) on the board that matches the player's selection
+  */
+  showMatchedLetter() {
+
+  }
+
+}
